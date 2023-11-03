fe457ad513e33dea3aed2a2a424e077e995dcfcc
Rextro <233275@ids.upchiapas.edu.mx>
2023-11-03 13:50:07 -0600

    adding security

--- BreederQr/src/main/resources/data.sql
+++ BreederQr/src/main/resources/data.sql
@@ -3,6 +3,6 @@ INSERT INTO `specie` (`id`, `name`)
 ON DUPLICATE KEY UPDATE name = 'Eublepharis macularius';
 
 INSERT INTO `breederqr`.`breeder` (`id`, `last_name`, `mail`, `name`, `password`, `second_last_name`, `username`)
-    VALUES (1, 'lastName', 'user@gmail.com', 'user1', '1234', 'secondLastName', 'user1')
+    VALUES (1, 'lastName', 'user@gmail.com', 'user1', '$2a$10$d7/7wXNHoLnKF8mlgoday.2iTBPqSuuJNzSDGaLKD69aH9TiMhQaq', 'secondLastName', 'user1')
 ON DUPLICATE KEY UPDATE name = 'user1';
 
